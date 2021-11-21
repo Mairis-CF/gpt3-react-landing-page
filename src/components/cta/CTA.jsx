26466a0ab25beeeec10158ed88ec738510a66722
Mairis <mairiscastroflorez@gmail.com>
2021-11-20 22:11:22 -0500

    terminando sección CTA y corrigiendo bugs en la navegación del sitio

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,9 +3,15 @@ import './cta.css';
 
 const CTA = () => {
     return (
-        <div>
-            <h1>CTA</h1>
-        </div>
+      <section className='gpt3__cta'>
+          <div className='gpt3__cta-content'>
+                <p>Request Early Access to Get Started</p>
+                <h4>Register today & start exploring the endless possiblities.</h4>
+          </div>
+          <div className='gpt3__cta-btn'>
+            <button type='button'>Get Started</button>
+          </div>
+      </section>
     )
 }
 
