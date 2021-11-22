0959a561a8446d8adae260793a248edf652f8d5d
Mairis <mairiscastroflorez@gmail.com>
2021-11-21 20:42:37 -0500

    Terminando sección de blog

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({ imgURL }) => {
+
+    const currentTime = Date.now();
+    const currentDate = new Date(currentTime).toDateString();
+
     return (
-        <div>
-            <h1>Article</h1>
+        <div className='gpt3__blog-container_article'>
+            <div className='gpt3__blog-container_article-image'>
+                <img src={imgURL} alt='Article blog' />
+            </div>
+
+            <div className='gpt3__blog-container_article-content'>
+                <div>
+                    <p>{currentDate}</p>
+                    <h3>GPT-3 and Open  AI is the future. Let us exlore how it is?</h3>
+                </div>
+                <p>Read Full Article</p>
+            </div>
         </div>
     )
 }
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import Article from '../../components/article/Article';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports'
 import './blog.css';
 
 const Blog = () => {
+
     return (
-        <div>
-            <h1>Blog</h1>
-        </div>
+        <section className='gpt3__blog section__padding' id='blog'>
+
+            <div className='gpt3__blog-heading'>
+                <h1 className='gradient__text'>A lot is happening,
+                    We are blogging about it.</h1>
+            </div>
+
+            <div className='gpt3__blog-container'>
+
+                <div className='gpt3__blog-container__groupA'>
+                    <Article imgURL={blog01}
+                    />
+                </div>
+
+
+                <div className='gpt3__blog-container__groupB'>
+                    <Article imgURL={blog02} />
+                    <Article imgURL={blog03} />
+                    <Article imgURL={blog04} />
+                    <Article imgURL={blog05} />
+                </div>
+            </div>
+
+        </section>
     )
 }
 
